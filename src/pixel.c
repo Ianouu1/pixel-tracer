14b22d3212c7e3648b0c8bc7b9de5237b978ba6f
Halim Djerroud <hdd@halim.info>
2023-03-02 17:03:50 +0100

    add square

--- src/pixel.c
+++ src/pixel.c
@@ -55,50 +55,55 @@ void pixel_point(Shape * shape, list * lst) {
     lst_insert_tail(lst, lst_create_lnode(px));
 }
 
+void draw_segment(int x, int y, int dx, int dy, Color color, list *lst){
+  int i, cumul;
+  int xinc, yinc;
+  Pixel *px;
+
+  xinc = (dx > 0) ? 1 : -1;
+  yinc = (dy > 0) ? 1 : -1;
+  dx = abs(dx);
+  dy = abs(dy);
+
+  px = create_pixel(x, y, color);
+  lst_insert_tail(lst, lst_create_lnode(px));
+
+  if (dx > dy) {
+    cumul = dx / 2;
+    for (i = 1; i <= dx; i++) {
+      x += xinc;
+      cumul += dy;
+      if (cumul >= dx) {
+        cumul -= dx;
+        y += yinc;
+      }
+      px = create_pixel(x, y, color);
+      lst_insert_tail(lst, lst_create_lnode(px));
+    }
+  } else {
+    cumul = dy / 2;
+    for (i = 1; i <= dy; i++) {
+      y += yinc;
+      cumul += dx;
+      if (cumul >= dy) {
+        cumul -= dy;
+        x += xinc;
+      }
+      px = create_pixel(x, y, color);
+      lst_insert_tail(lst, lst_create_lnode(px));
+    }
+  }
+}
+
 void pixel_line(Shape * shape, list * lst) {
     Line *p_line = (Line *) shape->ptrShape;
-    int dx, dy;
-    int i, x, y, cumul;
-    int xinc, yinc;
-    Pixel *px;
+    int dx, dy, x, y;
 
     x = p_line->p1->pos_x;
     y = p_line->p1->pos_y;;
     dx = p_line->p2->pos_x - p_line->p1->pos_x;
     dy = p_line->p2->pos_y - p_line->p1->pos_y;
-    xinc = (dx > 0) ? 1 : -1;
-    yinc = (dy > 0) ? 1 : -1;
-    dx = abs(dx);
-    dy = abs(dy);
-
-    px = create_pixel(x, y, shape->color);
-    lst_insert_tail(lst, lst_create_lnode(px));
-
-    if (dx > dy) {
-        cumul = dx / 2;
-        for (i = 1; i <= dx; i++) {
-            x += xinc;
-            cumul += dy;
-            if (cumul >= dx) {
-                cumul -= dx;
-                y += yinc;
-            }
-            px = create_pixel(x, y, shape->color);
-            lst_insert_tail(lst, lst_create_lnode(px));
-        }
-    } else {
-        cumul = dy / 2;
-        for (i = 1; i <= dy; i++) {
-            y += yinc;
-            cumul += dx;
-            if (cumul >= dy) {
-                cumul -= dy;
-                x += xinc;
-            }
-            px = create_pixel(x, y, shape->color);
-            lst_insert_tail(lst, lst_create_lnode(px));
-        }
-    }
+    draw_segment(x,y, dx,dy, shape->color,  lst);
 }
 
 void pixel_cercle(Shape * shape, list * lst) {
@@ -152,10 +157,25 @@ void pixel_cercle(Shape * shape, list * lst) {
 }
 
 void pixel_rectangle(Shape * shape, list * lst) {
-
+  printf("rec \n");
+  return;
 }
 
 void pixel_square(Shape * shape, list * lst) {
+  printf("square \n");
+  Squar *p_sqaure = (Squar *) shape->ptrShape;
+  int px1, py1, px2, py2, px3, py3, px4, py4;
+  int x, y, dx,dy;
+  Line l1, l2, l3, l4;
+
+  px1 = p_sqaure->p1->pos_x;
+  py1 = p_sqaure->p1->pos_y;
+  length = p_sqaure->length;
+
+  draw_segment(x,y, dx,dy, shape->color, lst);
+  draw_segment(x,y, dx,dy, shape->color, lst);
+  draw_segment(x,y, dx,dy, shape->color, lst);
+  draw_segment(x,y, dx,dy, shape->color, lst);
 
 }
 
