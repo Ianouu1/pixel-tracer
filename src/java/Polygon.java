d9c9c878ff584a9189ac99ebce3f51754ef68402
Florian <courrierdsf@gmail.com>
2025-05-05 12:12:03 +0200

    Avancement diagramme uml Shapes : Ajout de Polygone

--- src/java/Polygon.java
+++ src/java/Polygon.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 
 /**
  * Class Polygon
@@ -9,6 +9,8 @@ public class Polygon {
   // Fields
   //
 
+  private int n;
+  private Point[] points;
   
   //
   // Constructors
@@ -24,8 +26,49 @@ public class Polygon {
   // Accessor methods
   //
 
+  /**
+   * Set the value of n
+   * @param newVar the new value of n
+   */
+  public void setN (int newVar) {
+    n = newVar;
+  }
+
+  /**
+   * Get the value of n
+   * @return the value of n
+   */
+  public int getN () {
+    return n;
+  }
+
+  /**
+   * Set the value of points
+   * @param newVar the new value of points
+   */
+  public void setPoints (Point[] newVar) {
+    points = newVar;
+  }
+
+  /**
+   * Get the value of points
+   * @return the value of points
+   */
+  public Point[] getPoints () {
+    return points;
+  }
+
   //
   // Other methods
   //
+
+  /**
+   */
+  public void print()
+  {
+    System.out.println("n : " + n);
+    System.out.println("points : " + Arrays.toString(points));
+  }
+
 
 }
